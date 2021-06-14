dfd958d5ab0cee5d3ce6c31a6991ae8fae3af8e0
Bhawani Shekhawat <bhawani1988shekhawat@gmail.com>
2021-06-14 17:52:05 +0530

    added pwa install option

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { PromptComponent } from './prompt-component/prompt-component.component';
+import { PwaService } from './services/pwa.service';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+const initializer = (pwaService: PwaService) => () => pwaService.initPwaPrompt();
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
+    PromptComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
+    MatBottomSheetModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
     ServiceWorkerModule.register('./ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    NoopAnimationsModule
   ],
-  providers: [],
+  providers: [{provide: APP_INITIALIZER, useFactory: initializer, deps: [PwaService], multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/prompt-component/prompt-component.component.ts
@@ -0,0 +1,26 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bottom-sheet';
+
+@Component({
+  selector: 'app-prompt-component',
+  templateUrl: './prompt-component.component.html',
+  styleUrls: ['./prompt-component.component.scss']
+})
+export class PromptComponent implements OnInit {
+
+  constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: { mobileType: 'ios' | 'android', promptEvent?: any },
+              private bottomSheetRef: MatBottomSheetRef<PromptComponent>) { }
+
+  ngOnInit(): void {
+  }
+
+  public installPwa(): void {
+    this.data.promptEvent.prompt();
+    this.close();
+  }
+
+  public close(): void {
+    this.bottomSheetRef.dismiss();
+  }
+
+}
